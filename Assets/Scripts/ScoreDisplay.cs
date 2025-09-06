3a144ebb38ef10717a2ff8aff29503f9a95ef79f
Kailu <kailupucciarelli@yahoo.com>
2025-09-06 21:41:38 +0200

    Laser Defender

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    Text ScoreText;
+    GameSession gamesession;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText = GetComponent<Text>();
+        gamesession = FindObjectOfType<GameSession>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = gamesession.GetScore().ToString();
+    }
+}
